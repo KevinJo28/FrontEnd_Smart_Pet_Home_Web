cd881c3694b26473cdedc2f7277124f6a93212e7
KevinJo28 <kevinabarca1224@gmail.com>
2026-06-05 14:20:43 -0600

    fix(addDevices): fixed,  when you add device and not add

--- src/hooks/handleAddDevices.js
+++ src/hooks/handleAddDevices.js
@@ -1,19 +1,15 @@
 import { createDevice } from "../api/apiCreateDevice";
 export const handleAdd = async (e, deviceCode, deviceName, deviceType) => {
-  
+  e.preventDefault();
   try {
     const device = await createDevice({
       device_code: deviceCode,
       device_name: deviceName,
       device_type: deviceType,
     });
-
     console.log("Dispositivo creado:", device);
-
+    location.reload();
   } catch (error) {
     console.error(error);
   }
-  
-
-  
 };
